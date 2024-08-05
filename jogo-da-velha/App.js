ecd614236aa6fe16d1bb718fdf0b28576e0410be
ropokoJr <gustavomaganha112@gmail.com>
2024-08-05 11:46:02 -0300

    finalizado jogo da velha

--- jogo-da-velha/App.js
+++ jogo-da-velha/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, RefreshControl, ScrollView } from 'react-native';
 import Figura from './components/figura';
 import { useState } from 'react';
 
@@ -9,49 +9,88 @@ export default function App() {
   const [values, setValues] = useState({
     isCross: true,
     winMessage: ""
-  })
-  
+  });
+  const [Refresh, setRefresh] = useState(false);
+
+  const resetGame = () => {
+    setRefresh(true);
+    array.fill(0);
+    setValues({ isCross: true, winMessage: "" });
+    setRefresh(false);
+  }
+
   const changeMove = (number) => {
-    if (array[numbres] === 0 && !values.winMessage) {
+    if (array[number] === 0 && !values.winMessage) {
       array[number] = values.isCross;
-      setValues({ isCross: !values.isCross});
+      setValues({ isCross: !values.isCross });
+      winGame(number);
+    }
+  }
+
+  const winGame = (number) => {
+
+    if (
+      //linhas
+      (array[0] === array[number] && array[1] === array[number] && array[2] === array[number])
+      || (array[3] === array[number] && array[4] === array[number] && array[5] === array[number])
+      || (array[6] === array[number] && array[7] === array[number] && array[8] === array[number])
+
+      //colunas
+      || (array[0] === array[number] && array[3] === array[number] && array[6] === array[number])
+      || (array[1] === array[number] && array[4] === array[number] && array[7] === array[number])
+      || (array[2] === array[number] && array[5] === array[number] && array[8] === array[number])
+
+      //diagonais
+      || (array[0] === array[number] && array[4] === array[number] && array[8] === array[number])
+      || (array[2] === array[number] && array[4] === array[number] && array[6] === array[number])
+
+    ) {
+      setValues({ ...values, winMessage: array[number] ? "X Venceu" : "O Venceu" })
+    } else if (array.every((element) => element !== 0)) {
+      setValues({ ...values, winMessage: "Empate" });
     }
   }
 
   return (
-    <View style={styles.container}>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      refreshControl={
+        <RefreshControl Refreshing={Refresh} onRefresh={() => resetGame()} />
+      }
+    >
       <Text style={styles.text}>Jogo da Velha</Text>
       <View style={styles.row}>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={0}  clicado={() => changeMove(0)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={0} clicado={() => changeMove(0)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={1}  clicado={() => changeMove(1)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={1} clicado={() => changeMove(1)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={2}  clicado={() => changeMove(2)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={2} clicado={() => changeMove(2)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={3}  clicado={() => changeMove(3)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={3} clicado={() => changeMove(3)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={4}  clicado={() => changeMove(4)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={4} clicado={() => changeMove(4)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={5}  clicado={() => changeMove(5)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={5} clicado={() => changeMove(5)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={6}  clicado={() => changeMove(6)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={6} clicado={() => changeMove(6)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={7}  clicado={() => changeMove(7)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={7} clicado={() => changeMove(7)} />
         </View>
-      <View style={styles.box}>
-        <Figura vetor={array} posicao={8}  clicado={() => changeMove(8)}/>
+        <View style={styles.box}>
+          <Figura vetor={array} posicao={8} clicado={() => changeMove(8)} />
         </View>
       </View>
+      <Text style={styles.winMessage}>{values.winMessage}</Text>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -78,5 +117,11 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#000',
     padding: 10,
+  },
+  winMessage: {
+    fontSize: 30,
+    color: '#000',
+    fontWeight: 'bold',
+    margimTop: 20
   }
 });
